cf1cb8adbc520e50da7a8f175e63abd4b6636793
Adarsh <adarshmoger2000@gmail.com>
2025-07-29 18:06:01 +0530

    adding new dir to remote which is having terraform configurations

---
+++ three-files/02-file2.tf
@@ -0,0 +1,5 @@
+resource "local_file" "file2" {
+  filename = "file2.txt"
+  content  = "This is file2 created by Terraform."
+}
+
---
+++ three-files/03-file3.tf
@@ -0,0 +1,5 @@
+resource "local_file" "file3" {
+  filename = "file3.txt"
+  content  = "This is file3 created by Terraform."
+}
+
